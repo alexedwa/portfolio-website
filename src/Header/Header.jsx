944f2dcc5f121feb24e77f11b40e01af7f740f2f
alexedwa <alexedwa123@gmail.com>
2025-01-28 14:14:58 +0000

    Created Header

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -3,12 +3,15 @@ import styles from './Header.module.css'
 function Header() {
     
     return(
-        <ul className={styles.list}>
-            <li className={styles.listItems}><a href='#'>About</a></li>
-            <li className={styles.listItems}><a href='#'>Projects</a></li>
-            <li className={styles.listItems}><a href='#'>CV</a></li>
-            <li className={styles.listItems}><a href='https://github.com/alexedwa'>GitHub</a></li>
-        </ul>
+        <div className={styles.wrapper}>
+            <ul className={styles.list}>
+                <li className={styles.listItems}><a className={styles.linkText} href='#'>About</a></li>
+                <li className={styles.listItems}><a className={styles.linkText} href='#'>Projects</a></li>
+                <li className={styles.listItems}><a className={styles.linkText} href='#'>CV</a></li>
+                <li className={styles.listItems}><a className={styles.linkText} href='https://github.com/alexedwa'>GitHub</a></li>
+                <li className={styles.listItems}><a className={styles.linkText} href='#'>Contact</a></li>
+            </ul>
+        </div>
     );
 }
 
