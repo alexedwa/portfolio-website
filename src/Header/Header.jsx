2a7f1fe12229c6975cd95df082196301f141f614
alexedwa <alexedwa123@gmail.com>
2025-01-28 22:46:24 +0000

    Made header better

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,15 +1,14 @@
 import styles from './Header.module.css'
-
 function Header() {
-    
     return(
-        <div className={styles.wrapper}>
+        <div className={styles.navbar}>
+            <p className={styles.title}><a className={styles.linkText} href="#Top">Alexander Edwards</a></p>
             <ul className={styles.list}>
                 <li className={styles.listItems}><a className={styles.linkText} href='#'>About</a></li>
                 <li className={styles.listItems}><a className={styles.linkText} href='#'>Projects</a></li>
                 <li className={styles.listItems}><a className={styles.linkText} href='#'>CV</a></li>
-                <li className={styles.listItems}><a className={styles.linkText} href='https://github.com/alexedwa'>GitHub</a></li>
                 <li className={styles.listItems}><a className={styles.linkText} href='#'>Contact</a></li>
+                <li className={styles.listItems}><a className={styles.linkText} href='https://github.com/alexedwa'>GitHub</a></li>
             </ul>
         </div>
     );
