4d8b664f76581f5926c987862ddf0c736bf138bb
alexedwa <alexedwa123@gmail.com>
2025-01-29 20:53:57 +0000

    Added mobile support for header

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -2,16 +2,26 @@ import styles from './Header.module.css'
 function Header() {
     return(
         <div className={styles.navbar}>
-            <p className={styles.title}><a className={styles.linkText} href="#Top">Alexander Edwards</a></p>
+            <p className={styles.title}>Alexander Edwards</p>
             <ul className={styles.list}>
-                <li className={styles.listItems}><a className={styles.linkText} href='#'>About</a></li>
-                <li className={styles.listItems}><a className={styles.linkText} href='#'>Projects</a></li>
-                <li className={styles.listItems}><a className={styles.linkText} href='#'>CV</a></li>
-                <li className={styles.listItems}><a className={styles.linkText} href='#'>Contact</a></li>
-                <li className={styles.listItems}><a className={styles.linkText} href='https://github.com/alexedwa'>GitHub</a></li>
+                <li className={styles.listItems}><a className={styles.linkText} href='#About'>About</a></li>
+                <li className={styles.listItems}><a className={styles.linkText} href='#Projects'>Projects</a></li>
+                <li className={styles.listItems}><a className={styles.linkText} href='#CV'>CV</a></li>
+                <li className={styles.listItems}><a className={styles.linkText} href='#Contact'>Contact</a></li>
+                <li className={styles.listItems}><a className={styles.linkText} href='https://github.com/alexedwa' target='_blank'>GitHub</a></li>
             </ul>
+            <div className={styles.dropdown}>
+                <button className={styles.dropdownButton}>≡</button>
+                <div className={styles.dropdownContent}>
+                    <a href='#About'>About</a>
+                    <a href='#Projects'>Projects</a>
+                    <a href='#CV'>CV</a>
+                    <a href='#Contact'>Contact</a>
+                    <a href='https://github.com/alexedwa' target='_blank'>GitHub</a>
+                </div> 
+             </div>
         </div>
-    );
+    )
 }
 
 export default Header
