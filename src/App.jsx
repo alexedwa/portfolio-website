e7509632720a96d2fc00a1e5d2b9a5a1112db9e1
alexedwa <alexedwa123@gmail.com>
2025-01-31 22:12:53 +0000

    Recoloured background

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from './components/Header/Header.jsx'
 import About from './components/About/About.jsx'
 import Projects from './components/Projects/Projects.jsx'
 import Contact from './components/Contact/Contact.jsx'
+import Footer from './components/Footer/Footer.jsx'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <About />
       <Projects />
       <Contact />
+      <Footer />
     </>
   )
 }
