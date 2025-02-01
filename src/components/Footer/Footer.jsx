5467bcaa738d95b2930c7ccacf865a3d4f8a2103
alexedwa <alexedwa123@gmail.com>
2025-02-01 00:21:05 +0000

    Fixed image overlap issue with footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from './Footer.module.css'
 function Footer() {
     return(
         <div className={styles.footerContainer}>
-            <a href='https://github.com/alexedwa'><img className={styles.image} src='/src/assets/github-mark-white.svg' alt='GitHub logo and link to GitHub profile' width={80} height={80}></img></a>
+            <a href='https://github.com/alexedwa' target='_blank' className={styles.image}></a>
         </div>
     )
 }
